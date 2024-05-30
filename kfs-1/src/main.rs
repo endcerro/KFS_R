77ae4d9b18b0c01ea6e7355c5e1351d09da7152d
Enzo Dal Cerro <enzodalcerro@gmail.com>
2024-05-30 17:54:42 +0200

    boot

--- kfs-1/src/main.rs
+++ kfs-1/src/main.rs
@@ -1,14 +1,24 @@
 #![no_std]
 #![no_main]
+// #![feature(asm)]
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"victor salope";
 
 #[no_mangle]
 pub extern "C" fn _start() -> !
 {
-    loop {
+    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
     }
+
+    loop {}
 }
 
 #[panic_handler]
