193f9fda8aa69c08aa6eb977fe9d050554a9d745
Enzo Dal Cerro <edal--ce@student.42.fr>
2024-06-03 20:33:57 +0200

    clean

--- kfs-1/install.sh
+++ kfs-1/install.sh
@@ -1,30 +1,13 @@
 #! /bin/bash
 
-
-function binutils () 
-{
-    cd
-    export PREFIX="$HOME/cross"
-    export TARGET=i686-elf
-    export PATH="$PREFIX/bin:$PATH"
-    git clone git://sourceware.org/git/binutils-gdb.git cross
-    cd 
-    mkdir build-binutils
-    cd build-binutils
-    ../cross/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-    make
-    make install
-    cd - 
-}
-
 function rust ()
 {
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 function deps ()
 {
-    sudo apt install -y build-essential make  nasm grub-common xorriso grub-pc-bin
+    sudo apt install -y build-essential make curl nasm grub-common xorriso grub-pc-bin qemu-system-x86
 }
 
-rust
 deps
+rust
